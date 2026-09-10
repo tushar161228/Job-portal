50ebeb2115d2106287cc6dd8d7f98e8f3d0b6ae2
Tushar Verma <tv161228@gmail.com>
2026-09-10 14:48:54 +0530

    Updated applucation Routes in backend

--- backend/controllers/application.controller.js
+++ backend/controllers/application.controller.js
@@ -29,7 +29,96 @@ export const applyJobs=async(req,res)=>{
             applicant:userId,
         });
 
-        job.applications.push
+        job.applications.push(newApplication._id);
+        await job.save();
+        return res.status(200).json({
+            message:"Job applied successfully",
+            success:true,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+export const getAppliedJobs=async(req,res)=>{
+    try {
+        const userId=req.id;
+        const application=await Application.find({applicant:userId}).sort({createdAt:-1}).populate({
+            path:'job',
+            options:{sort:{createdAt:-1}},
+            populate:{
+                path:'company',
+                options:{sort:{createdAt:-1}},
+            }
+        });
+        if(!application){
+            return res.status(404).json({
+                message:"No application",
+                success:false,
+            });
+        }
+        return res.status(200).json({
+            application,
+            success:true,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+export const getApplicants=async(req,res)=>{
+    try {
+        const jobId=req.params.id;
+        const job=await Job.findById(jobId).populate({
+            path:'applications',
+            options:{sort:{createdAt:-1}},
+            populate:{
+                path:'applicant'
+            }
+        });
+        if(!job){
+            return res.status(404).json({
+                message:"Job not found",
+                success:false,
+            });
+        }
+
+        return res.status(200).json({
+            job,
+            success:true,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+export const updateStatus=async(req,res)=>{
+    try {
+        const{status}=req.body;
+        const applicationId=req.params.id;
+        if(!status){
+            return res.status(400).json({
+                message:"status is required",
+                success:false,
+            });
+        }
+        
+        const application =await Application.findOne({_id:applicationId});
+        if(!application){
+            return res.status(400).json({
+                message:"Application not found",
+                success:false,
+            }); 
+        }
+        application.status=status.toLoweCase();
+        await application.save();
+        return res.status(200).json({
+            message:"Status updated successfully",
+            success:true,
+        })
     } catch (error) {
         console.log(error);
     }
---
+++ backend/routes/application.route.js
@@ -0,0 +1,11 @@
+import express from "express";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+import { applyJobs, getApplicants, getAppliedJobs, updateStatus } from "../controllers/application.controller.js";
+const router=express.Router();
+
+router.route("/apply/:id").get(isAuthenticated,applyJobs);
+router.route("/get").get(isAuthenticated,getAppliedJobs);
+router.route("/:id/applicants").get(isAuthenticated,getApplicants);
+router.route("/status/:id/update").post(isAuthenticated,updateStatus);
+
+export default router;
