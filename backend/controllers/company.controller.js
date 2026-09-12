d6aeaa630150de4519acbb16af8c277d58fd0a7d
Tushar Verma <tv161228@gmail.com>
2026-09-12 10:27:23 +0530

    Updated backend code

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -1,4 +1,4 @@
-import Company from "../models/company.model.js";
+import {Company} from "../models/company.model.js";
 export const registerCompany=async(req,res)=>{
     try {
         const {companyName}=req.body;
--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -1,4 +1,4 @@
-import { Job } from "../models/job.model";
+import { Job } from "../models/job.model.js";
 export const postjob = async (req, res) => {
   try {
     const {
